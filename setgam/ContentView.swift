845dbd07e4346eac16b492093339b14bf605825b
Zhe Liu <zheliu@Admins-iMac-5.local>
2020-07-20 21:52:42 -0700

    first commit

---
+++ setgam/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  setgam
+//
+//  Created by Zhe Liu on 7/20/20.
+//  Copyright © 2020 Zhe Liu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
